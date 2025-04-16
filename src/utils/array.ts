f2f16335b67e73963252351ec448d9d1334df9fb
gepardoj <anna.ra.phoenix@gmail.com>
2025-04-16 18:57:09 +0300

    get continuous intervals

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,3 +1,23 @@
-export function getIntervals(arr: number[]) {
-  return "0-";
+/**
+ * 
+ * @param arr unsorted array of number, i.e. [0,2,1,3,4,5,8,9,11]
+ * @returns string containing a list of continuous intervals, i.e. "0-5,8-9,11"
+ */
+export function getContinuousIntervals(arr: number[]): string {
+  if (arr.length === 0) return "";
+  const intervals: string[] = [];
+  const sorted = [...arr].sort((a, b) => a - b);
+  intervals.push(`${sorted[0]}`);
+  let isAnyGap = false;
+  for (let i = 1; i < sorted.length; i++) {
+    const prev = sorted[i - 1];
+    const curr = sorted[i];
+    if (curr - prev > 1) {
+      intervals[intervals.length - 1] += `-${prev}`;
+      intervals.push(`${curr}`);
+      isAnyGap = true;
+    }
+  }
+  if (!isAnyGap && sorted.length > 1) intervals[0] += `-${sorted[sorted.length - 1]}`
+  return intervals.join(",")
 }
--- test/utils/array.test.ts
+++ test/utils/array.test.ts
@@ -1,5 +1,8 @@
-import { getIntervals } from "../../src/utils/array"
+import { getContinuousIntervals } from "../../src/utils/array"
 
-test('get intervals equal ', () => {
-  expect(getIntervals([])).toBe("0-");
+test('continuous intervals are correct', () => {
+  expect(getContinuousIntervals([1, 4, 5, 2, 3, 9, 8, 11, 0])).toBe("0-5,8-9,11");
+  expect(getContinuousIntervals([1, 4, 3, 2])).toBe("1-4");
+  expect(getContinuousIntervals([1])).toBe("1");
+  expect(getContinuousIntervals([])).toBe("");
 });
